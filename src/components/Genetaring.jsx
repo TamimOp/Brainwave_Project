43379e8df03f7e11c583055181b492bdfad1fd40
TamimOp <tamimshadanik@gmail.com>
2024-07-04 04:33:39 +0600

    Final Deploy

---
+++ src/components/Genetaring.jsx
@@ -0,0 +1,14 @@
+import { loading } from "../assets";
+
+const Genetaring = ({ className }) => {
+  return (
+    <div
+      className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] text-base ${className} || ""`}
+    >
+      <img src={loading} alt="loading" className="w-5 h-5 mr-4" />
+      AI is generating
+    </div>
+  );
+};
+
+export default Genetaring;
